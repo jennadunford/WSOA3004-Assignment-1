f993b730a7f885563503875f676e4c4fb42907fd
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-08-02 12:42:36 +0200

    instantiates a randomly sized block

--- Assets/JennaAssets/JennaScripts/instantiatestuffonthetilemap.cs
+++ Assets/JennaAssets/JennaScripts/instantiatestuffonthetilemap.cs
@@ -13,49 +13,26 @@ public class instantiatestuffonthetilemap : MonoBehaviour
     void Start()
     {
         Vector3Int position;
-        //position = new Vector3Int(0, 0, 0);
-        //gameTiles.SetTile(position,paletteTiles[0]);
-        //Vector3Int position;
-        //position = new Vector3Int(0, 0, 0);
-        //addTile(0, position);
 
-
-        for (int x = 0; x < Random.Range(5, 10); x++)
-        {
-            //position = new Vector3Int(x+1, x + 1, x+5);
-            //addTile(Random.Range(0, 2), position);
-            //position = new Vector3Int(x + 4, x + 1, x-2);
-            //addTile(Random.Range(0, 2), position);
-            //position = new Vector3Int(x, x+1, x-4);
-            //addTile(Random.Range(0, 2), position);
-            //position = new Vector3Int(-x, -x, -x);
-            //addTile(Random.Range(0, 2), position);
-            //make a row of tiles
-            position = new Vector3Int(x, x, x);
-            addTile(Random.Range(0, 2), position);
-            position = new Vector3Int(x, x + 1, x);
-            addTile(Random.Range(0, 2), position);
-            position = new Vector3Int(x, x + 2, x);
-            addTile(Random.Range(0, 2), position);
-            position = new Vector3Int(x, x + 3, x);
-            addTile(Random.Range(0, 2), position);
-            position = new Vector3Int(x, x + 4, x);
-            addTile(Random.Range(0, 2), position);
-
-        }
-        for (int x = 0; x < 20; x++)
-        {
-            position = new Vector3Int(Random.Range(-10, 11), Random.Range(-10, 11), Random.Range(-10, 11));
-            addTile(Random.Range(0, 2), position);
-        }
-
-        // Update is called once per frame
-      
-
-        void addTile(int tileNum, Vector3Int position)
-        {
-            gameTiles.SetTile(position, paletteTiles[tileNum]);
-        }
-    } 
+        int xpos = 0;
+        int ypos = 0;
+        int rand = Random.Range(5, 11);
+        int rand2 = Random.Range(5, 11);
+            for (int i = 0; i <= rand; i++)
+            {
+            Debug.Log("first for loop");
+                for (int j = 0; j <= rand2; j++)
+                {
+                    position = new Vector3Int(xpos + i, ypos + j, 0);
+                    addTile(Random.Range(0, 2), position);
+                }
+            }
+       
+        
+    }
+    void addTile(int tileNum, Vector3Int position)
+    {
+        gameTiles.SetTile(position, paletteTiles[tileNum]);
+    }
 }
 
