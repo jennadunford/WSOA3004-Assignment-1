a89fa18fe472d52387b3f80be5c469a9468c749d
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-08-10 00:16:51 +0200

    Revert "Lose Screen"
    This reverts commit 64a6dba0f4176933cf4fcad1471b74412b6f0e01.

--- Assets/MusaWork/Scripts/PlayerScore.cs
+++ Assets/MusaWork/Scripts/PlayerScore.cs
@@ -5,24 +5,11 @@ using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour
 {
-    public Text timerText;
-    public Text highScoreText;
-    public static int scoreCounter = 0;
-    public static int highScoreCount = 0;
-    public static float rawTime = 0;
+    public Transform player;
+    public Text scoreText;
 
-    public static bool gameEnd = false;
-
-    // Update is called once per frame
     void Update()
     {
-        if (!gameEnd)
-        {
-            //rawTime += Time.deltaTime;
-            //scoreCounter = ((int)rawTime) * 10;
-            scoreCounter += (int)(Mathf.Ceil(Time.deltaTime));
-            timerText.text = scoreCounter.ToString();
-            highScoreText.text = highScoreCount.ToString();
-        }
+        scoreText.text = player.position.y.ToString("0");
     }
 }
