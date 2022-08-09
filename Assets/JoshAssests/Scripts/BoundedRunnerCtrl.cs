50ebfb1f7ed46e51ed3916b94529d1faed7d3595
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-08-10 00:30:33 +0200

    Update BoundedRunnerCtrl.cs

--- Assets/JoshAssests/Scripts/BoundedRunnerCtrl.cs
+++ Assets/JoshAssests/Scripts/BoundedRunnerCtrl.cs
@@ -7,6 +7,7 @@ public class BoundedRunnerCtrl : MonoBehaviour
     [SerializeField] PlayerBounds bounds;
     [SerializeField] private float forwardSpeed = 1f, shiftForce = 1f, maxShiftSpeed = 1000f, outDrag = 1f, maxOutSpeed, outSpin = 10f;
     [SerializeField] string shiftAxis = "Horizontal";
+    [SerializeField] string shiftAxis = "Horizontal", secondAxis = "Vertical";
 
     private bool ready = true;
     public bool IsRunning { get; private set; } = false;
